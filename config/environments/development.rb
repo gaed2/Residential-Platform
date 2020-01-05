3b644bd6efb03eaed8537139c2d353a60d99c617
Imran <imranfazley@gmail.com>
2020-01-05 12:19:06 +0800

    initial mailer setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,7 @@ config.webpacker.check_yarn_integrity = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
       address:              Figaro.env.SMTP_ADDRESS,
