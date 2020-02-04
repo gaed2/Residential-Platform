38606d1060fa79af9a88f9848410af8b29f760fb
Imran <imranfazley@gmail.com>
2020-02-04 17:54:39 +0800

    add peak off peak top3 plans

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,9 +8,11 @@ class HomesController < ApplicationController
                                          .order(created_at: :desc)
                                          .includes(city: [:region], property_sub_category: [:property_category])
                                          .paginate(page: page, per_page: Constant::PROPERTY_PER_PAGE)
-    @fixed_plans = SuppliersPlan.where(plan_type: "fixed").first(3)
-    @discounted_plans = SuppliersPlan.where(plan_type: "discount_off").first(3)
-    @peakoffpeak_plans = PeakAndOffPeakPlan.first(3)
+    @fixed_plans = SuppliersPlan.where(plan_type: "fixed")
+                                .order(:price).first(3)
+    @discounted_plans = SuppliersPlan.where(plan_type: "discount_off")
+                                     .order(:price).first(3)
+    @peakoffpeak_plans = PeakAndOffPeakPlan.order(:peak_price).first(3)
   end
 
   def drafts_only
